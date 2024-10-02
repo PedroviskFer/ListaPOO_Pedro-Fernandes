5ef89a34086a6bc1e0063ea32f2e506fc3ffdaca
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-02 17:46:52 -0300

    exercicio2
    Arquivos do exercicio 2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado = new Quadrado();
+
+            Console.WriteLine("Quanto é a aresta do quadrado?");
+            int aresta = int.Parse(Console.ReadLine());
+
+            quadrado.SetAresta(aresta);
+            quadrado.CalArea();
+
+            Console.WriteLine("Segundo o que colocou, a área do quadrado vai ser:"+quadrado.GetArea());         
+           
+            Console.ReadLine();
+        }
+    }
+}
+
+    
+
---
+++ Quadrado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex2
+{
+    internal class Quadrado
+    {
+        private int aresta;
+        private int area;
+        public void SetAresta(int aresta)
+        {
+            this.aresta = aresta;
+        }
+        public int GetAresta()
+        {
+            return this.aresta;
+        }
+        public int GetArea()
+        {
+            return this.area;
+        }
+        public void CalArea()
+        {
+            this.area = this.aresta * this.aresta;
+        }
+    }
+}
