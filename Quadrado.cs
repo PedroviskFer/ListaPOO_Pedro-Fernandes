363b843eb282307cd1397837adf1c02b1b9d73d2
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-02 20:52:48 -0300

    Add files via upload
    Arquivos do exercicio 2

---
+++ Quadrado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex2
+{
+    internal class Quadrado
+    {
+        private int aresta;
+        private int area;
+
+        public void SetAresta(int aresta)
+        {
+            this.aresta = aresta;
+        }
+        public int GetAresta()
+        {
+            return this.aresta;
+        }
+        public int GetArea()
+        {
+            return this.area;
+        }
+        public void CalcularA()
+        {
+            this.area = (this.aresta * this.aresta) ;
+        }
+    }
+}
