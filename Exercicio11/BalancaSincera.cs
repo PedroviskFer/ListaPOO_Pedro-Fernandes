ce3f25bae3cbeb4ce9050f65bf0041e6d3216f33
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-03 14:40:16 -0300

    Add files via upload

---
+++ Exercicio11/BalancaSincera.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_11
+{
+    internal class BalancaSincera
+    {   private int peso;
+        private int altura;
+        private int relacao;
+        public void SetPesoEAltura(int peso, int altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+        public void CalcularRelacao()
+        {
+            this.relacao = peso/((altura/ 100)* (altura / 100)); 
+        }
+        public string VerificarPesoIdeal()
+        {
+            if (relacao<20)
+            {
+                return "Abaixo do peso (Tá na hora de comer mais, hein Gatao?)";
+            }
+            else if (relacao >= 20&&relacao < 25)
+            {
+                return "Peso ideal (Olha você aí, no ponto!)";
+            }
+            else
+            {
+                return "Acima do peso (A balança chorou, bora cuidar, meu amigo!)";
+            }
+        }
+    }
+}
+ 
---
+++ Exercicio11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_11
+{ /*11. Entrar com o peso e a altura de uma determinada pessoa. Após a digitação, exibir se esta
+pessoa está ou não com seu peso ideal. Veja tabela da relação peso/altura². */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  BalancaSincera balanca = new BalancaSincera();
+
+            Console.WriteLine("Digite o seu peso (sem virgula, meu nobre):");
+            int peso = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a sua altura (em cm):");
+            int altura = Convert.ToInt32(Console.ReadLine());
+
+            balanca.SetPesoEAltura(peso, altura);
+            balanca.CalcularRelacao();
+            string resultado = balanca.VerificarPesoIdeal();
+            Console.WriteLine(resultado);
+        }
+    }
+}
