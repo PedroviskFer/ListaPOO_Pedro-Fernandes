369293393ca0232b85bf1dfa09358764e7099e01
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-02 22:09:51 -0300

    Add files via upload

---
+++ Exercicio 04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ex04
+{
+    internal class Program
+    { /* 4. A partir dos valores da base e altura de um triângulo, calcular e exibir sua área. */
+        static void Main(string[] args)
+        { Triangulo triangulo = new Triangulo();
+
+            Console.WriteLine("coloque a base do seu triangulo");
+            int b = int.Parse(Console.ReadLine());
+            triangulo.setBase (b);
+
+            Console.WriteLine("Agora coloque a altura do seu triangulo");
+            int h = int.Parse(Console.ReadLine());
+
+            triangulo.setAltura (h);
+            triangulo.Calcularea();
+
+            Console.WriteLine("A area do seu triangulo é:" + triangulo.getArea());
+            Console.Write("...");
+
+
+
+        }
+    }
+}
---
+++ Exercicio 04/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ex04
+{ /*4. A partir dos valores da base e altura de um triângulo, calcular e exibir sua área. */
+    internal class Triangulo
+    {
+        private int b;
+        private int h;
+        private int area;
+
+        public void setAltura(int h)
+        { this.h = h; 
+        }
+        public void setBase(int b)
+        {
+            this.b = b;
+        }
+        public int getBase()
+            { return this.b; 
+        }
+        public int getAltura()
+        {
+            return this.h;
+        }
+        public int getArea()
+        {
+            return this.area;
+        }
+        public void Calcularea()
+            { this.area = (b * h) / 2; ; 
+        }
+
+
+    }
+}
