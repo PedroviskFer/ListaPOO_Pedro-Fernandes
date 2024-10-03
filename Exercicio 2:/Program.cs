f5e1e36186166f9c03153977ec91a172067d730d
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-02 21:24:57 -0300

    Exercicios 2

---
+++ Exercicio 2:/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadradito = new Quadrado();
+
+            Console.WriteLine("escreva o valor da aresta do quadrado");
+            int aresta = int.Parse(Console.ReadLine());
+
+            quadradito.SetAresta(aresta);
+            quadradito.CalcularA();
+
+            Console.WriteLine("A área vai ser:" + quadradito.GetArea());
+            Console.ReadLine();
+        }
+    }
+}
