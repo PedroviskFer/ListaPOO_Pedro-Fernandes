b4b3972986ce33eaa17af4e1c6e020d7767f7239
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-03 01:37:54 -0300

    Add files via upload

---
+++ ex8./Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._8
+{
+    internal class Program
+    { /* 8. Entrar via teclado, com dois valores distintos. Exibir o maior deles. */
+        static void Main(string[] args)
+        {  Comparador comparador = new Comparador();
+
+            Console.WriteLine("COMPARADOR DE VALORES VALOROSOS****:Digite o primeiro valor:");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+
+            comparador.SetValores(valor1, valor2);
+            comparador.CalcularMaior();
+
+            Console.WriteLine("O maior valor é: " + comparador.GetMaiorValor());
+        }
+    }
+}
---
+++ ex8./comparador.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._8
+{
+    internal class Comparador
+    {   private int valor1;
+        private int valor2;
+        private int maiorValor;
+        /* 8. Entrar via teclado, com dois valores distintos. Exibir o maior deles. */
+        public void SetValores(int v1, int v2)
+        {
+            this.valor1 = v1;
+            this.valor2 = v2;
+        }
+
+        public void CalcularMaior()
+        {
+            if (valor1 > valor2)
+            {
+                maiorValor = valor1;
+            }
+            else
+            {
+                maiorValor = valor2;
+            }
+        }
+
+        public int GetMaiorValor()
+        {
+            return maiorValor;
+        }
+    }
+}
