15576fb40ccfc1d1236d2b3f180191f0b3e02146
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-03 01:46:43 -0300

    Add files via upload

---
+++ Exercicio09/Comparador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_09
+{
+    internal class Comparador
+    {   private int valor1;
+        private int valor2;
+        public void SetValores(int v1, int v2)
+        {
+            this.valor1 = v1;
+            this.valor2 = v2;
+        }
+        public string VerificarMaior()
+        {
+            if (valor1 > valor2)
+            {
+                return "O maior valor é: " + valor1;
+            }
+            else if (valor2 > valor1)
+            {
+                return "O maior valor é: " + valor2;
+            }
+            else
+            {
+                return "Os números são idênticos.";
+            }
+        }
+    }
+}
---
+++ Exercicio09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparador comparador = new Comparador();
+
+            Console.WriteLine("Digite o primeiro valor:");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+
+            comparador.SetValores(valor1, valor2);
+            string resultado = comparador.VerificarMaior();
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
