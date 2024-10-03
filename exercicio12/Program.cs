3ffe1981e96dd45d661f045502415405bafc7b91
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-03 14:48:26 -0300

    Add files via upload

---
+++ exercicio12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   Triangulo triangulo = new Triangulo();       
+            Console.WriteLine("Digite o valor do primeiro lado:");
+            int lado1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do segundo lado:");
+            int lado2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do terceiro lado:");
+            int lado3 = Convert.ToInt32(Console.ReadLine());
+            
+            triangulo.SetLados(lado1, lado2, lado3);
+            
+            if (triangulo.EhTriangulo())
+            {
+                Console.WriteLine("Formam um triângulo! " + triangulo.ClassificarTriangulo());
+            }
+            else
+            {
+                Console.WriteLine("Esses lados não formam um triângulo! (Tenta de novo!)");
+            }
+        }
+    }
+}       
---
+++ exercicio12/Triangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_12
+{
+    internal class Triangulo
+    {   private int lado1;
+        private int lado2;
+        private int lado3;
+        public void SetLados(int l1, int l2, int l3)
+        {
+            this.lado1 = l1;
+            this.lado2 = l2;
+            this.lado3 = l3;
+        }
+        public bool EhTriangulo()
+        { return (lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1);
+        }
+        public string ClassificarTriangulo()
+        {
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                return "Triângulo Equilátero (Todos os lados iguais, coisa rara hein!)";
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                return "Triângulo Isósceles (Dois lados iguais, tá quase simétrico!)";
+            }
+            else
+            {
+                return "Triângulo Escaleno (Nenhum lado igual, confusão total!)";
+            }
+        }
+    }
+}
+   
