fd7ec5f195973f0cb5280dc5cf01fdabf59f0cf1
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-03 01:27:53 -0300

    Add files via upload

---
+++ ex.7/Cotação.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_ex7
+{
+    internal class Cotação
+    {/*7.Entrar via teclado com o valor da cotação do dólar e uma certa quantidade de dólares.
+Calcular e exibir o valor correspondente em Reais(R$)*/
+        private int cotacaoDoDolar;
+        private int quantidadeDeDolares;
+        private int valorEmReais;
+
+        public void SetCotacaoDoDolar(int cotacao)
+        {
+            this.cotacaoDoDolar = cotacao;
+        }
+
+        public void SetQuantidadeDeDolares(int quantidade)
+        {
+            this.quantidadeDeDolares = quantidade;
+        }
+
+        public void CalcularValorEmReais()
+        {
+            this.valorEmReais = this.cotacaoDoDolar * this.quantidadeDeDolares;
+        }
+
+        public int GetValorEmReais()
+        {
+            return this.valorEmReais;
+        }
+    }
+}
---
+++ ex.7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_ex7
+{
+    internal class Program
+    {/*7.Entrar via teclado com o valor da cotação do dólar e uma certa quantidade de dólares.
+Calcular e exibir o valor correspondente em Reais(R$)*/
+        static void Main(string[] args)
+        {
+            Cotação cotacao = new Cotação();
+
+            Console.WriteLine("Qual é a cotação do dólar? (Respira fundo antes de responder):");
+            int cotacaoDoDolar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Quantos dólares você tem? (Se não tem, é só chorar):");
+            int quantidadeDeDolares = Convert.ToInt32(Console.ReadLine());
+
+            cotacao.SetCotacaoDoDolar(cotacaoDoDolar);
+            cotacao.SetQuantidadeDeDolares(quantidadeDeDolares);
+
+            cotacao.CalcularValorEmReais();
+
+            Console.WriteLine("O valor em reais é: R$ " + cotacao.GetValorEmReais() + " (Melhor não olhar muito!)");
+        }
+    }
+}
