031238c314fcaeebb0063f021e8f599965f46661
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-02 21:26:35 -0300

    Add files via upload

---
+++ Exercicio3_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadradao = new Quadrado();
+
+            Console.WriteLine("Quanto é a diagonal (hipotenusa) do quadrado?");
+            double diagonal = double.Parse(Console.ReadLine());
+
+            quadradao.SetDiagonal(diagonal);
+            quadradao.CalcularArea();
+
+            Console.WriteLine("Segundo o que colocou, a área do quadrado vai ser:"+ quadradao.GetArea());
+            Console.ReadLine();
+        }
+    }
+}
+    
