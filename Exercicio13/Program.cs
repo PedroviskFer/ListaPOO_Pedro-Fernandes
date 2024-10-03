645b456dd53b46296c70971c521e3df4ac41c1e7
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-03 15:30:30 -0300

    Add files via upload

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_13
+{
+    internal class Program
+    { /*13. Verificar se três valores quaisquer (A,B, C) que serão digitados formam ou não um
+triângulo retângulo. Lembre-se que o quadrado da hipotenusa é igual a soma dos quadrados dos
+catetos. */
+        static void Main(string[] args)
+        {  TriangulaMatematicoeDoido triangulo = new TriangulaMatematicoeDoido();
+            Console.WriteLine("Pitagoras está querendo cheirar o seu triangulo para ver se aprova.Digite o valor do primeiro lado (A):");
+           
+            Console.WriteLine("Digite o valor do primeiro lado (A):");
+            int ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do segundo lado :(B):");
+            int ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do terceiro lado por favore  (C):");
+            int ladoC = Convert.ToInt32(Console.ReadLine());
+
+            triangulo.SetLados(ladoA, ladoB, ladoC);
+
+            if (triangulo.EhTrianguloRetangulo())
+            {
+                Console.WriteLine("Você tem um triângulo retângulo nas mãos! (Pitagoras está orgulhoso! Ele vai sair do Tumulo só para tomar um belo café melita com você)");
+            }
+            else
+            {
+                Console.WriteLine("Não é um triângulo retângulo! (Pitagoras desaprova e revira do tumulo!)");
+            }
+        }
+    }
+}
---
+++ Exercicio13/TriangulaMatematicoeDoido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_13
+{/*13. Verificar se três valores quaisquer (A,B, C) que serão digitados formam ou não um
+triângulo retângulo. Lembre-se que o quadrado da hipotenusa é igual a soma dos quadrados dos
+catetos. */
+    internal class TriangulaMatematicoeDoido
+    {   private int a;
+        private int b;
+        private int c;
+        public void SetLados(int ladoA, int ladoB, int ladoC)
+        {   this.a = ladoA;
+            this.b = ladoB;
+            this.c = ladoC;
+        }
+        public bool EhTrianguloRetangulo()
+        {          
+            return (a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b);
+        }
+    }
+}
