c1f86f978fe1dcc945814e55150530e290243561
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-02 23:37:26 -0300

    Add files via upload

---
+++ Ex_6/Milhas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOOEx6_
+{ /* Sabendo que uma milha marítima equivale a um mil, oitocentos e cinqüenta e dois metros
+e que um quilômetro possui mil metros, fazer um programa para converter milhas marítimas em
+quilômetros. */
+    internal class Milhas
+    {
+        private int m;
+        private double km;
+
+        public void SetMilhas(int m)
+        {
+            this.m = m;
+        }
+        public int GetMilhas()
+        {
+            return this.m;
+        }
+        public double GetQuilometro()
+        {
+            return this.km;
+        }
+
+        public void Calcular()
+        {
+            this.km = (this.m * 1852) / 1000.00;
+        }
+    }
+}
+
---
+++ Ex_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOOEx6_
+{ /* Sabendo que uma milha marítima equivale a um mil, oitocentos e cinqüenta e dois metros
+e que um quilômetro possui mil metros, fazer um programa para converter milhas marítimas em
+quilômetros. */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Milhas milhas = new Milhas();
+
+            Console.WriteLine("CONVERSOR DE MILHAS EM QUILÔMETROS");
+            Console.WriteLine("Coloque as milhas que você quer converter:");
+
+            int quantidadeMilhas = int.Parse(Console.ReadLine());
+            milhas.SetMilhas(quantidadeMilhas);
+            milhas.Calcular();
+            Console.WriteLine("...girando engrenagens internas");
+            Console.WriteLine ("CONVERSOR DIGITALMENTE DIGITAL em kms:"+ milhas.GetQuilometro());
+            Console.WriteLine("...Bip BOp Bum, a porta se abriu!!!!");
+           
+        }
+    }
+}
