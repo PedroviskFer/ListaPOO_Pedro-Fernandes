eb80b9f90e02bcbef3f66fb83b11936bf516c4cd
PedroviskFer <166468373+PedroviskFer@users.noreply.github.com>
2024-10-03 14:20:20 -0300

    Add files via upload

---
+++ Exercicio 10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_10
+{
+    internal class Program
+    {/*10. Calcular e exibir a área de um retângulo, a partir dos valores da base e altura que serão
+digitados.Se a área for maior que 100, exibir a mensagem “Terreno grande”, caso contrário, exibir
+a mensagem “Terreno pequeno”*/
+        static void Main(string[] args)
+        {  Terreninho terreninho = new Terreninho();
+
+            Console.WriteLine("Digite o valor da base do retângulo (A base do seu terreninho):");
+            int baseRetangulo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o a altura do seu terreinho :");
+            int alturaRetangulo = Convert.ToInt32(Console.ReadLine());
+
+            terreninho.SetValores(baseRetangulo, alturaRetangulo);
+            terreninho.CalcularArea();
+
+            int area = terreninho.GetArea();
+            Console.WriteLine("A área do retângulo é: " + area);
+
+            string classificacao = terreninho.ClassificarTerreno();
+            Console.WriteLine(classificacao);
+        }
+    }
+}
---
+++ Exercicio 10/Terreninho.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_10
+{/*10. Calcular e exibir a área de um retângulo, a partir dos valores da base e altura que serão
+digitados.Se a área for maior que 100, exibir a mensagem “Terreno grande”, caso contrário, exibir
+a mensagem “Terreno pequeno”*/
+    internal class Terreninho
+    {   private int baseRetangulo;
+        private int alturaRetangulo;
+        private int area;
+        public void SetValores(int baseRet, int alturaRet)
+        {
+            this.baseRetangulo = baseRet;
+            this.alturaRetangulo = alturaRet;
+        }
+        public void CalcularArea()
+        {
+            this.area = baseRetangulo * alturaRetangulo;
+        }
+        public int GetArea()
+        {
+            return this.area;
+        }
+        public string ClassificarTerreno()
+        {
+            if (area > 100)
+            {
+                return "Terreno grande";
+            }
+            else
+            {
+                return "Terreno pequeno";
+            }
+        }
+    }
+}
+
